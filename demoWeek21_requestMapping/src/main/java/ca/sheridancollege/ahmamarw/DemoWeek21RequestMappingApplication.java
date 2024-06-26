50e3959d7b6db44484ccfeafd27f58cdbb75fb52
Marwa Ahmadzai <marwa7271483@gmail.com>
2024-06-25 20:36:55 -0400

    Student Registration!

---
+++ demoWeek21_requestMapping/src/main/java/ca/sheridancollege/ahmamarw/DemoWeek21RequestMappingApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.ahmamarw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeek21RequestMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeek21RequestMappingApplication.class, args);
+	}
+
+}
